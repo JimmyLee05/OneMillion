31ade3f823fbf706f8586fdb17913ff8cd36f291
JimmyLee05 <NanJun05@163.com>
2017-12-18 12:00:26 +0800

    add new files

---
+++ Test.py
@@ -0,0 +1,30 @@
+ func didClickInfoView(isClickMenu: Bool = false) {
+        if mynt?.isOwner == false && !AppConfig.canShareEdit { return }
+
+        guard let contentView = contentView else { return }
+        contentView.infoView.rotateArrow(isExpand: contentView.infoView.subview == nil)
+        if contentView.infoView.subview == nil {
+            contentView.scrollView.expand(contentView.editView, below: contentView.infoView)
+        } else {
+            contentView.scrollView.shrink(contentView.infoView)
+        }
+        if isClickMenu && contentView.infoView.subview != nil {
+            contentView.scrollView.setContentOffset(.zero, animated: true)
+        }
+    }
+
+
+func didClickInfoView(isClickMenu: Bool = false) {
+    if mynt?.isOwner == false && !AppConfig.canShareEdit { return }
+
+    guard let contentView = contentView else { return }
+    contentView.infoView.rotateArrow(isExpand: contentView.infoView.subview == nil)
+    if contentView.infoView.subview == nil {
+        contentView.scrollView.expand(contentView.editView, below: contentView.infoView)
+    } else {
+        contentView.scrollView.shrink(contentView.infoView)
+    }
+    if isClickMenu && contentView.infoView.subview != nil {
+        contentView.scrollView.setContentOffset(.zero, animated: true)
+    }
+}
