81434378fe2f530ec171c376f3622aaa6339dd81
JimmyLee05 <NanJun05@163.com>
2017-12-20 15:53:45 +0800

    add new files

--- Test.py
+++ Test.py
@@ -1,34 +1,46 @@
- func didClickInfoView(isClickMenu: Bool = false) {
-        if mynt?.isOwner == false && !AppConfig.canShareEdit { return }
-
-        guard let contentView = contentView else { return }
-        contentView.infoView.rotateArrow(isExpand: contentView.infoView.subview == nil)
-        if contentView.infoView.subview == nil {
-            contentView.scrollView.expand(contentView.editView, below: contentView.infoView)
-        } else {
-            contentView.scrollView.shrink(contentView.infoView)
+@objc func updateTipsImage() {
+    guard let mynt = mynt else { return }
+    // mynt规则
+    func useMyntRule() {
+        switch mynt.uiState {
+        case .online:
+            //蓝牙在线，随机显示
+            updateRandomTips([.phoneAlarm, .myntAlarm])
+        case .connecting:
+            tips = .connecting
+        default:
+            let ruleHandler = { [weak self] in
+                if let mynt = self?.mynt {
+                    if Int(Date().timeIntervalSince1970) - mynt.lastDisconnectTime < 3600 {
+                        // 断线时间小于1小时
+                        self?.updateRandomTips([.walk, .map])
+                    } else {
+                        //断线时间缠裹一小时
+                        if mynt.lostState == .reportLost {
+                            //已报丢
+                            self?.updateRandomTips([.map, .askhelp])
+                        } else {
+                            //未报丢
+                            self?.updateRandomTips([.walk, .notice])
+                        }
+                    }
+                }
+            }
+            // 进行经纬度请求，检测是否在500米以内，显示walk
+            requestLocation { [weak self] location in
+                if let mynt = self?.mynt,
+                    let location = location {
+                    if self?.tips = .walk && location.coordinate.distance(from: mynt.coordinate) < 500 {
+                        self?.tips = .walk
+                        return
+                    }
+                }
+            }
+            ruleHandler()
         }
-        if isClickMenu && contentView.infoView.subview != nil {
-            contentView.scrollView.setContentOffset(.zero, animated: true)
-        }
     }
 
-
-func didClickInfoView(isClickMenu: Bool = false) {
-    if mynt?.isOwner == false && !AppConfig.canShareEdit { return }
-
-    guard let contentView = contentView else { return }
-    contentView.infoView.rotateArrow(isExpand: contentView.infoView.subview == nil)
-    if contentView.infoView.subview == nil {
-        contentView.scrollView.expand(contentView.editView, below: contentView.infoView)
-    } else {
-        contentView.scrollView.shrink(contentView.infoView)
-    }
-    if isClickMenu && contentView.infoView.subview != nil {
-        contentView.scrollView.setContentOffset(.zero, animated: true)
-    }
+    
 }
-
-
 
 
