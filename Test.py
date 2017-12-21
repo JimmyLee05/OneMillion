16cce1ca8d751a7d6a40d2eb306ed3dd79ae6550
JimmyLee05 <NanJun05@163.com>
2017-12-21 17:54:47 +0800

    Test some fentures

--- Test.py
+++ Test.py
@@ -1,63 +1,18 @@
-@objc func updateTipsImage() {
-    guard let mynt = mynt else { return }
-    // mynt规则
-    func useMyntRule() {
-        switch mynt.uiState {
-        case .online:
-            //蓝牙在线，随机显示
-            updateRandomTips([.phoneAlarm, .myntAlarm])
-        case .connecting:
-            tips = .connecting
-        default:
-            let ruleHandler = { [weak self] in
-                if let mynt = self?.mynt {
-                    if Int(Date().timeIntervalSince1970) - mynt.lastDisconnectTime < 3600 {
-                        // 断线时间小于1小时
-                        self?.updateRandomTips([.walk, .map])
-                    } else {
-                        //断线时间缠裹一小时
-                        if mynt.lostState == .reportLost {
-                            //已报丢
-                            self?.updateRandomTips([.map, .askhelp])
-                        } else {
-                            //未报丢
-                            self?.updateRandomTips([.walk, .notice])
-                        }
-                    }
-                }
-            }
-            // 进行经纬度请求，检测是否在500米以内，显示walk
-            requestLocation { [weak self] location in
-                if let mynt = self?.mynt,
-                    let location = location {
-                    if self?.tips = .walk && location.coordinate.distance(from: mynt.coordinate) < 500 {
-                        self?.tips = .walk
-                        return
-                    }
-                }
-            }
-            ruleHandler()
+// 加入断线位置
+        if mynt != nil && mynt?.coordinate.isNull == false {
+            let disconnectLocation = SCDevice.SCLocation(latitude: mynt!.latitude, longitude: mynt!.longitude, time: mynt!.lastDisconnectTime)
+            sourceLocations.append(disconnectLocation)
+        } else {
+            // 没有位置
+            locationLabel.text  = MTLocalizedString("MYNTSETTING_MAP_NODATA", comment: "没有数据")
+            dateLabel.text      = MTLocalizedString("MYNTSETTING_MAP_NODATA", comment: "没有数据")
         }
-    }
 
-}
-
-
-private extension SCDevice.SCLocation.LocType {
-    
-    var typeImage: UIImage? {
-        switch self {
-        case .gps:
-            return UIImage(named: "map_type_gps")
-        case .mobile:
-            return UIImage(named: "map_type_mobile")
-        case .station:
-            return UIImage(named: "map_type_station")
-        default:
-            return nil
+//加入断线位置
+        if mynt != nil && mynt?.coordinate.isNull == false {
+            let disconnectLocation = SCDevice.SCLocation(latitude: mynt!.latitude, longitude: mynt!.longitude, time: mynt!.lastDisconnectTime)
+        } else {
+            // 没有位置
+            locationLabel.text = MTLocalizedString("MYNTSETTING_MAP_NODATA", comment: "没有数据")
+            dateLabel.text     = MTLocalizedString("MYNTSETTING_MAP_NODATA", comment: "没有数据")
         }
-    }
-}
-
-
-
