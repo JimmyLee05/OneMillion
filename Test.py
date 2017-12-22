10e86813038beee1c6744d70e2023c79c769a167
JimmyLee05 <NanJun05@163.com>
2017-12-22 12:58:33 +0800

    Test some fentures

--- Test.py
+++ Test.py
@@ -1,16 +1,20 @@
-@objc func didClickNavigationButton(_ sender: AnyObject) {
-        if selectLocation?.coordinate.isNull == false {
-            MapNavigationKit.shared.selectMapApp(fromCoordinate: userCoordinate,
-                                                 toCoordinate: selectLocation!.coordinate,
-                                                 view: view)
-        }
+func showMenu() {
+        let text = isShowPath ? MTLocalizedString("HIDE_STRACK", comment: "隐藏轨迹") : MTLocalizedString("SHOW_STRACK", comment: "显示轨迹")
+        let actionSheet = UIActionSheet(title: nil,
+                                        delegate: self,
+                                        cancelButtonTitle: MTLocalizedString("SECURE_AREA_DELETE_CANCEL", comment: "取消"),
+                                        destructiveButtonTitle: nil,
+                                        otherButtonTitles: text)
+        actionSheet.show(in: view)
     }
 
 
-@objc func didClickNavigationButton(_ sender: AnyObject) {
-        if selectLocation?.coordinate.isNull == false {
-            MapNavigationKit.shared.selectMapApp(fromCoordinate: userCoordinate,
-                                                 toCoordinate: selectLocation!.coordinate,
-                                                 view: view)
-        }
+func showMenu() {
+        let text = isShowPath ? MTLocalizedString("HIDE_STRACK", comment: "隐藏轨迹") : MTLocalizedString("SHOW_STRACK", comment: "显示轨迹")
+        let actionSheet = UIActionSheet(title: nil,
+                                        delegate: self,
+                                        cancelButtonTitle: MTLocalizedString("SECURE_AREA_DELETE_CANCEL", comment: "取消"),
+                                        destructiveButtonTitle: nil,
+                                        otherButtonTitles: text)
+        actionSheet.show(in: view)
 }
