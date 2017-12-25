aa4b4b0a88755fde3d413920cf21f8870cbb1ae7
JimmyLee05 <NanJun05@163.com>
2017-12-25 15:55:09 +0800

    Test some fentures]

--- Test.py
+++ Test.py
@@ -1,20 +1,38 @@
-func showMenu() {
-        let text = isShowPath ? MTLocalizedString("HIDE_STRACK", comment: "隐藏轨迹") : MTLocalizedString("SHOW_STRACK", comment: "显示轨迹")
-        let actionSheet = UIActionSheet(title: nil,
-                                        delegate: self,
-                                        cancelButtonTitle: MTLocalizedString("SECURE_AREA_DELETE_CANCEL", comment: "取消"),
-                                        destructiveButtonTitle: nil,
-                                        otherButtonTitles: text)
-        actionSheet.show(in: view)
+fileprivate func _initRipple(_ phoneImage: UIImage) {
+        let count = 5
+        let rippleWidth: CGFloat = phoneImage.size.width * 0.8
+        for _ in 0..<count {
+            let layer           = CAShapeLayer()
+            layer.contentsScale = UIScreen.main.scale
+            layer.bounds        = CGRect(x: 0, y: 0, width: rippleWidth, height: rippleWidth)
+            layer.anchorPoint   = CGPoint(x: 0.5, y: 0.5)
+            // 光圈颜色等待确定修改
+            layer.strokeColor   = UIColor.lightGray.cgColor
+            layer.fillColor     = UIColor.clear.cgColor
+            layer.lineWidth     = 1
+            layer.path          = UIBezierPath(ovalIn: layer.bounds).cgPath
+            layer.opacity       = 0
+            contentView.layer.insertSublayer(layer, at: 0)
+            rippleLayers.append(layer)
+        }
     }
 
-
-func showMenu() {
-        let text = isShowPath ? MTLocalizedString("HIDE_STRACK", comment: "隐藏轨迹") : MTLocalizedString("SHOW_STRACK", comment: "显示轨迹")
-        let actionSheet = UIActionSheet(title: nil,
-                                        delegate: self,
-                                        cancelButtonTitle: MTLocalizedString("SECURE_AREA_DELETE_CANCEL", comment: "取消"),
-                                        destructiveButtonTitle: nil,
-                                        otherButtonTitles: text)
-        actionSheet.show(in: view)
+fileprivate func _initRipple(_ phoneImage: UIImage) {
+    
+    let count = 5
+    let rippleWidth: CGFloat = phoneImage.size.width * 0.8
+    for _ in 0..<count {
+        let layer            = CAShapeLayer()
+        layer.contentsScale  = UIScreen.main.scale
+        layer.bounds         = CGRect(x: 0, y: 0, width: rippleWidth, height: rippleWidth)
+        layer.anchorPoint    = CGPoint(x: 0.5, y: 0.5)
+        // 光圈颜色等待确定修改
+        layer.strokeColor    = UIColor.lightGray.cgColor
+        layer.fillColor      = UIColor.clear.cgColor
+        layer.lineWidth      = 1
+        layer.path           = UIBezierPath(ovalIn: layer.bounds).cgPath
+        layer.opacity        = 0
+        contentView.layer.insertSublayer(layer, at: 0)
+        rippleLayers.append(layer)
+    }
 }
