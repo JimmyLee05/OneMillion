e8eb2f99600f75c27d4ace63fca7b364ad20b28b
JimmyLee05 <NanJun05@163.com>
2017-12-27 11:24:55 +0800

    Test some fentures]

--- Test.py
+++ Test.py
@@ -1,32 +1,60 @@
-fileprivate extension SCDeviceType {
+extension ShareListViewController {
     
-    var image: UIImage? {
-        switch self {
-        case .none:
-            return nil
-        case .mynt:
-            return UIImage(named: "app_product_add_mynt")
-        case .myntGPS:
-            return UIImage(named: "app_product_add_myntgps")
-        case .myntES:
-            return UIImage(named: "app_product_add_myntes")
+    // 数据源
+    func reloadFriendList() {
+        friends = []
+        mynt?.shareUsers(success: { [weak self] friends in
+            self?.friends = friends
+            self?.tableView.mj_header.endRefreshing()
+            }, failure: { [weak self] _, message in
+                if self?.friends.isEmpty == true {
+                    self?.friends = []
+                }
+                MTToast.show(message)
+        })
+    }
+    
+    // 停止分享弹框
+    func didClickDeleteDialog(friend: SCFriend) {
+        let message = MTLocalizedString("GPS_SHARE_DELETE_MESSAGE", comment: "")
+        DialogManager.shared.show(text: message) { [weak self] _ in
+            self?.didClickStopShareButton(friend: friend)
         }
+    }
+    
+    func didClickStopShareButton(friend: SCFriend) {
+        guard let mynt = self.mynt else { return }
+        if friends.isEmpty { return }
+        mynt.stopShare(userId: friend.friendId, success: { [weak self] in
+            self?.friends.remove(object: friend)
+            
+            }, failure: { _, message in
+                MTToast.show(message)
+        })
     }
     
 }
 
-fileprivate extension SCDeviceType {
+extension ShareListViewController {
     
-    var image UIImage? {
-        switch self {
-        case .none:
-            return nil
-        case .mynt:
-            return UIImage(named: "app_product_add_mynt")
-        case .myntGPS:
-            return UIImage(named: "app_product_add_myntgps")
-        case .myntES:
-            return UIImage(named: "app_product_add_myntes")
-        }
+    // 数据源
+    func reloadFriendList() {
+        friends = []
+        mynt?.shareUsers(success: { [weak self] friends in
+            self?.friends = friends
+            self?.tableView.mj_header.endRefreshing()
+            }, failure: { [weak self] _, message in
+                if self?.friends.isEmpty == true {
+                    self?.friends = []
+                }
+                MTToast.show(message)
+        })
+    }
+
+    // 停止分享弹框
+    func didClickDeleteDialog(friend: SCFriend) {
+        let message = MTLocalizedString("GPS_SHARE_DELETE_MESSAGE", comment: "")
+        DialogManager.shared.show(text: message) { [weak self] _ in
+            self?.didClickStopShareButton(friends: friend)}
     }
 }
