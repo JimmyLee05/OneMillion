3101fc48fe97f8a87a01b43a2bfc81979841fad8
JimmyLee05 <NanJun05@163.com>
2017-12-27 15:34:15 +0800

    Test some fentures]

--- Test.py
+++ Test.py
@@ -1,60 +1,29 @@
-extension ShareListViewController {
-    
-    // 数据源
-    func reloadFriendList() {
-        friends = []
-        mynt?.shareUsers(success: { [weak self] friends in
-            self?.friends = friends
-            self?.tableView.mj_header.endRefreshing()
-            }, failure: { [weak self] _, message in
-                if self?.friends.isEmpty == true {
-                    self?.friends = []
-                }
-                MTToast.show(message)
-        })
-    }
-    
-    // 停止分享弹框
-    func didClickDeleteDialog(friend: SCFriend) {
-        let message = MTLocalizedString("GPS_SHARE_DELETE_MESSAGE", comment: "")
-        DialogManager.shared.show(text: message) { [weak self] _ in
-            self?.didClickStopShareButton(friend: friend)
+var click: [SCClickValue] {
+        switch self {
+        case .camera:
+            return [.cameraShutter]
+        case .music:
+            return [.musicPlay, .musicNext, .musicPrevious]
+        case .phone:
+            return []
+        case .ppt:
+            return [.pptNextPage, .pptPreviousPage]
+        default:
+            return []
         }
     }
-    
-    func didClickStopShareButton(friend: SCFriend) {
-        guard let mynt = self.mynt else { return }
-        if friends.isEmpty { return }
-        mynt.stopShare(userId: friend.friendId, success: { [weak self] in
-            self?.friends.remove(object: friend)
-            
-            }, failure: { _, message in
-                MTToast.show(message)
-        })
-    }
-    
-}
 
-extension ShareListViewController {
-    
-    // 数据源
-    func reloadFriendList() {
-        friends = []
-        mynt?.shareUsers(success: { [weak self] friends in
-            self?.friends = friends
-            self?.tableView.mj_header.endRefreshing()
-            }, failure: { [weak self] _, message in
-                if self?.friends.isEmpty == true {
-                    self?.friends = []
-                }
-                MTToast.show(message)
-        })
-    }
-
-    // 停止分享弹框
-    func didClickDeleteDialog(friend: SCFriend) {
-        let message = MTLocalizedString("GPS_SHARE_DELETE_MESSAGE", comment: "")
-        DialogManager.shared.show(text: message) { [weak self] _ in
-            self?.didClickStopShareButton(friends: friend)}
-    }
+var click: [SCClickValue] {
+        switch self {
+        case .camera:
+            return [.cameraShutter]
+        case .music:
+            return [.musicPlay, .musicNext, .musicPrevious]
+        case .phone:
+            return []
+        case .ppt:
+            return [.pptNextPage, .pptPreviousPage]
+        default:
+            return []
+        }
 }
