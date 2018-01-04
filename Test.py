9c0a511816d2e4696d4d2a2aa030e2bb8ad99eaa
JimmyLee05 <NanJun05@163.com>
2018-01-04 14:30:14 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,40 +1,21 @@
-func formatTime(time: Int, ignoreState: Bool = false) -> String {
-        // 不满60分钟   xxx minute ago
-        // 不满24小时   xxx hour ago
-        // 超过24小时   xxx day ago
-        let minute: Double = 60
-        let hour: Double   = minute * 60
-        let day: Double   = hour * 24
-        let dValue: Double = Date().timeIntervalSince1970 - Double(time)
-
-        if state == .connected && !ignoreState {
-            return MTLocalizedString("DISCONNECT_TIME_NOW", comment: "now")
-        }
-
-        if time <= 0 {
-            return ""
-        }
-        if dValue <= minute {
-            return MTLocalizedString("DISCONNECT_TIME_NOW", comment: "now")
-        }
-        switch dValue {
-        case 0..<hour:
-            return String(format: MTLocalizedString("DISCONNECT_TIME_MINUTE", comment: "分钟"), dValue / minute)
-        case hour..<day:
-            return String(format: MTLocalizedString("DISCONNECT_TIME_HOUR", comment: "小时"), dValue / hour)
-        default:
-            return String(format: MTLocalizedString("DISCONNECT_TIME_DAY", comment: "天"), dValue / day)
+ var selectedType: SelectedType = .left {
+        didSet {
+            let selectedLeft = selectedType == .left
+            
+            leftView.backgroundColor = selectedLeft ? UIColor(red: 0.91, green: 0.91, blue: 0.91, alpha: 1.00) : UIColor.clear
+            rightView.backgroundColor = !selectedLeft ? UIColor(red: 0.91, green: 0.91, blue: 0.91, alpha: 1.00) : UIColor.clear
+            leftHookImageView.isHidden  = !selectedLeft
+            rightHookImageView.isHidden = selectedLeft
         }
     }
 
-func format(time: Int, ignoreState: Bool = false) -> String {
-    // 不满60分钟 xxx minute ago
-    // 不满24小时 xxx hour ago
-    // 超过24小时 xxx day ago
-    let minute: Double = 60
-    let hour: Double   = minute * 60
-    let day: Double    = hour * 24
-    let dValue: Double = Date().timeIntervalSince1970 - Double(time)
+var selectedType: SelectedType = .left {
+        didSet {
+            let selectedLeft = selectedType == .left
 
-    
+            leftView.backgroundColor    = selectedLeft ? UIColor(red: 0.91, green: 0.91, blue: 0.91, alpha: 1.00) : UIColor.clear
+            rightView.backgroundColor   = !selectedLeft ? UIColor(red: 0.91, green: 0.91, blue: 0.91, alpha: 1.00) : UIColor.clear
+            leftHookImageView.isHidden  = !selectedLeft
+            rightHookImageView.isHidden = selectedLeft
+        }
 }
