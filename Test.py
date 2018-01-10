8244789d74e9f7765f1e63c4625e35f5e7bd43f3
JimmyLee05 <NanJun05@163.com>
2018-01-10 09:44:50 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,22 +1,36 @@
-if UIApplication.shared.applicationState == .background {
-            // 后台进行推送
-            let message = String(format: MTLocalizedString("MYNTSETTING_INFO_LOWPOWER_DIALOG_MESSAGE", comment: ""), mynt.name)
-            STJPushKit.shared().pushLocalNotification(nil,
-                                                      subtitle: nil,
-                                                      body: message,
-                                                      identifier: mynt.sn,
-                                                      sound: nil,
-                                                      userInfo: ["sn": mynt.sn])
+fileprivate func loadDefaultAvatar(_ block: (() -> Void)? = nil) {
+        let usage = self.usage
+        // 开始绘图
+        DispatchQueue.global().async { [weak self] in
+            guard let mynt = self else { return }
+            let image = usage.myntAvatar(mynt: mynt)
+            let color = usage.usageColor
+
+            // 开始绘制 120 x 120 大小的图片
+            let avatar = UIImage.create(with: 120, icon: image, color: color)
+
+            DispatchQueue.main.async { [weak self] in
+                self?.avatar = avatar?.round()
+                block?()
+            }
         }
+    }
 
+fileprivate func loadDefaultAvatar(_ block: (() -> Void)? = nil) {
+        let usage = self.usage
+        // 开始绘图
+        DispatchQueue.global().async { [weak self] in
+            guard let mynt = self else { return }
+            let image = usage.myntAvatar(mynt: mynt)
+            let color = usage.usageColor
 
-if UIApplication.shared.applicationState == .background {
-            // 后台进行推送
-            let message = String(format: MTLocalizedString("MYNTSETTING_INFO_LOWPOWER_DIALOG_MESSAGE", comment: ""), mynt.name)
-            STJPushKit.shared().pushLocalNotification(nil,
-                                                      subtitle: nil,
-                                                      body: message,
-                                                      identifier: mynt.sn,
-                                                      sound: nil,
-                                                      userInfo: ["sn": mynt.sn])
-}
+            // 开始绘制 120 x 120 大小的图片
+            let avatar = UIImage.create(with: 120, icon: image, color: color)
+
+            DispatchQueue.main.async { [weak self] in
+                self?.avatar = avatar?.round()
+                block?()
+            }
+        }
+    }
+
