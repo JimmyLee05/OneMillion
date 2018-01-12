56b2d122e0cc89cee75e37b4030d980c368ef1ae
JimmyLee05 <NanJun05@163.com>
2018-01-12 10:12:21 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,53 +1,26 @@
-public class func login(email: String,
-                            password: String,
-                            success: @escaping (SCUser, Bool) -> Void,
-                            failure: SCFailedHandler) {
-        let param: [String: Any] = ["email": email,
-                                    "pass": password.md5]
-        post(url: "user/login", auth: false, param: param, success: { (json) in
-            let user = Json2User(email: email, json: json)
-            let check = json["login_device_check"].int
-
-            if !SCloud.shared.isMYNTAPP {
-                user.save()
-                success(user, false)
-                return
-            }
-            if check != nil && check == 1 {
-                tempUser = user
-                success(user, true)
-            } else {
-                user.save()
-                success(user, false)
-            }
-        }, failure: failure)
+ class func updateToken(token: String) {
+        let user = currentUser()
+        user?.token = token
+        user?.save()
     }
 
-public class func login(email: String,
-                        password: String,
-                        success: @escaping (SCUser, Bool) -> Void,
-                        failure: SCFailedHandler) {
-        let param: [String: Any] = ["email": email,
-                                    "pass":password.md5]
-        post(url: "user/login", auth: false, param: param, success: { (json) in
-            let user = Json2User(email: email, json: json)
-            let check = json["login_device_check"].int
-
-            if !SCloud.shared.isMYNTAPP {
-                user.save()
-                success(user, false)
-                return
-            }
-            if check != nil && check == 1 {
-                tempUser = user
-                success(user, true)
-            } else {
-                user.save()
-                success(user, false)
-            }
-        }, failure: failure)
+class func updateAvatar(avatar: String) {
+    let user = currentUser()
+    user?.avatar = avatar
+    user?.save()
 }
 
+class func updateToken(token: String) {
+        let user = currentUser()
+        user?.token = token
+        user?.save()
+}
+
+class func updateAvatar(avatar: String) {
+    let user = currentUser()
+    user?.avatar = avatar
+    user?.save()
+}
 
 
 
