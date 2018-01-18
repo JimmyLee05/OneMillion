5ecbf041877bd1f8eda5bd4a9c8fb3adce6252fb
JimmyLee05 <NanJun05@163.com>
2018-01-18 09:36:53 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,18 +1,90 @@
-protocol MTCoverFlowViewDelegate: NSObjectProtocol {
+import UIKit
 
-    func numberOfItemsInCoverFlowViewn(_ collectionView: MTCoverFlowView) -> Int
+class CrashViewController: BaseViewController {
 
-    func coverFlowView(_ coverFlowView: MTCoverFlowView, cellForItemAt index: Int, cell: MTCoverFlowView.CoverFlowViewCell)
+    override var isShowBackgroundLayer: Bool { return false }
 
-    func coverFlowView(_ coverFlowView: MTCoverFlowView, didSelectItemAt index: Int)
+    @IBOutlet weak var fixButton: GradientButton!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    var cancelHandler: ((UIViewController?) -> Void)?
+
+    var fixedHandler: ((UIViewController?) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Crash"
+        setLeftBarButtonItem(image: Resource.Image.Navigation.close)
+
+        fixButton.setButtonBackgroundColorStyle(ColorStyle.kBlueGradientColor)
+        fixButton.setTitle(NSLocalizedString("CRASH_FIX", comment: ""), for: .normal)
+        messageLabel.text = NSLocalizedString("CRASH_MESSAGE", comment: "")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func leftBarButtonClickedHandler() {
+        AppConfig.isSimulatedCrash = false
+        dismiss(animated: true)
+        cancelHandler?(self)
+    }
+
+    @IBAction func didClickFixButton(_ sender: Any) {
+        CrashUtils.deleteAllFilesUnderDocumentsLibraryCaches()
+        dismiss(animated: true)
+        fixedHandler?(self)
+    }
 
 }
 
-protocol MTCoverFlowViewDelegate: NSObjectProtocol {
+import UIKit
+
+class CrashViewController: BaseViewController {
     
-    func numberOfItemsInCoverFlowViewn(_ collectionView: MTCoverFlowView) -> Int
+    override var isShowBackgroundLayer: Bool { return false }
 
-    func coverFlowView(_ coverFlowView: MTCoverFlowView, cellForItemAt index: Int, cell: MTCoverFlowView.CoverFlowViewCell)
+    @IBOutlet weak var fixButton: GradientButton!
+    @IBOutlet weak var messageLabel: UILabel!
 
-    func coverFlowView(_ coverFlowView: MTCoverFlowView, didSelectItemAt index: Int)
+    var cancelHandler: ((UIViewController?) -> Void)?
+
+    var fixedHandler: ((UIViewController?) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Crash"
+        setLeftBarButtonItem(image: Resource.Image.Navigation.close)
+
+        fixButton.setButtonBackgroundColorStyle(ColorStyle.kBlueGradientColor)
+        fixButton.setTitle(NSLocalizedString("CRASH_FIX", comment: ""). for: .normal)
+        messageLabel.text = NSLocalizedString("CRASH_MESSAGE", comment: "")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func leftBarButtonClickedHandler() {
+        AppConfig.isSimulatedCrash = false
+        dismiss(animated: true)
+        cancelHandler?(self)
+    }
+
+    @IBAction func didClickFixButton(_ sender: Any) {
+        CrashUtils.deleteAllFilesUnderDocumentsLibraryCaches()
+        dismiss(animated: true)
+        fixedHandler?(self)
+    }
 }
+
+
+
+
+
+
+
+
+
