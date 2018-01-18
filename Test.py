0dae6ebf1429713a0c18886222e156a1353122c0
JimmyLee05 <NanJun05@163.com>
2018-01-18 14:45:05 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,90 +1,29 @@
-import UIKit
 
-class CrashViewController: BaseViewController {
+extension HomeViewController {
 
-    override var isShowBackgroundLayer: Bool { return false }
-
-    @IBOutlet weak var fixButton: GradientButton!
-    @IBOutlet weak var messageLabel: UILabel!
-
-    var cancelHandler: ((UIViewController?) -> Void)?
-
-    var fixedHandler: ((UIViewController?) -> Void)?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Crash"
-        setLeftBarButtonItem(image: Resource.Image.Navigation.close)
-
-        fixButton.setButtonBackgroundColorStyle(ColorStyle.kBlueGradientColor)
-        fixButton.setTitle(NSLocalizedString("CRASH_FIX", comment: ""), for: .normal)
-        messageLabel.text = NSLocalizedString("CRASH_MESSAGE", comment: "")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func leftBarButtonClickedHandler() {
-        AppConfig.isSimulatedCrash = false
-        dismiss(animated: true)
-        cancelHandler?(self)
-    }
-
-    @IBAction func didClickFixButton(_ sender: Any) {
-        CrashUtils.deleteAllFilesUnderDocumentsLibraryCaches()
-        dismiss(animated: true)
-        fixedHandler?(self)
+    // 选中
+    func didSelectTableView(mynt: Mynt?) {
+        MyntInfoViewController.push(mynt: mynt, parentViewController: self)
     }
 
 }
 
-import UIKit
-
-class CrashViewController: BaseViewController {
+extension HomeViewController {
     
-    override var isShowBackgroundLayer: Bool { return false }
-
-    @IBOutlet weak var fixButton: GradientButton!
-    @IBOutlet weak var messageLabel: UILabel!
-
-    var cancelHandler: ((UIViewController?) -> Void)?
-
-    var fixedHandler: ((UIViewController?) -> Void)?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Crash"
-        setLeftBarButtonItem(image: Resource.Image.Navigation.close)
-
-        fixButton.setButtonBackgroundColorStyle(ColorStyle.kBlueGradientColor)
-        fixButton.setTitle(NSLocalizedString("CRASH_FIX", comment: ""). for: .normal)
-        messageLabel.text = NSLocalizedString("CRASH_MESSAGE", comment: "")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    override func leftBarButtonClickedHandler() {
-        AppConfig.isSimulatedCrash = false
-        dismiss(animated: true)
-        cancelHandler?(self)
-    }
-
-    @IBAction func didClickFixButton(_ sender: Any) {
-        CrashUtils.deleteAllFilesUnderDocumentsLibraryCaches()
-        dismiss(animated: true)
-        fixedHandler?(self)
+    // 选中
+    func didSelectTableView(mynt: Mynt?) {
+        MyntInfoViewController.push(mynt: mynt, parentViewController: self)
     }
 }
 
+func myntKit(myntKit: MYNTKit, didAddMynt mynt: Mynt) {
+        // 新增设备
+        homeListView?.tableView.reloadData()
+        updateMapView()
+    }
 
-
-
-
-
-
-
-
+func myntKit(myntKit: MYNTKit, didAddMynt mynt: Mynt) {
+        // 新增设备
+        homeListView?.tableView.reloadData()
+        updateMapView()
+}
