4c4a0629f66ec2e05b546385ca3b69bf9cb76195
JimmyLee05 <NanJun05@163.com>
2018-01-19 14:51:25 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,11 +1,21 @@
- func didClickBuyMynt() {
-        WebViewController.present(self,
-                                  title: MTLocalizedString("LBL_SETTING_STORE", comment: ""),
-                                  url: buyMyntURL)
+ fileprivate func _initInfoView() {
+        /* 添加箭头茅点 */
+        arrowImageView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        /* 初始化手势 */
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didClickInfoView))
+        infoView.addGestureRecognizer(tapGestureRecognizer)
     }
 
-func didClickBuyMynt() {
-        WebViewController.present(self,
-                                  title: MTLocalizedString("LBL_SETTING_STORE", comment: ""),
-                                  url; buyMyntURL)
+fileprivate func _initInfoView() {
+        /* 添加箭头茅点 */
+        arrowImageView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        /* 初始化手势 */
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didClickInfoView))
+        infoView.addGestureRecognizer(tapGestureRecognizer)
 }
+
+albumView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didClickEditThumb(tapGestureRecognizer:))))
+
+albumView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didClickEditThumb(tapGestureRecognizer:)))
