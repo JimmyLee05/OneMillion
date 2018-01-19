dba5755f24aaadc0bd5ab3504f07ce8c16ffa123
JimmyLee05 <NanJun05@163.com>
2018-01-19 10:08:23 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,32 +1,80 @@
-fileprivate extension SCDeviceType {
-    
-    var image: UIImage? {
-        switch self {
-        case .mynt:
-            return UIImage(named: "list_mynt")
-        case .myntGPS:
-            return UIImage(named: "list_gps")
-        case .myntES:
-            return UIImage(named: "list_es")
-        case .none:
-            return nil
+class RoundedRectangleMaskLayer: CAShapeLayer {
+
+    override var cornerRadius: CGFloat {
+        didSet {
+            setNeedsDisplay()
+            layoutIfNeeded()
         }
     }
+    override var bounds: CGRect {
+        didSet {
+            setNeedsDisplay()
+            layoutIfNeeded()
+        }
+    }
+
+    override init() {
+        super.init()
+        contentsScale = UIScreen.main.scale
+        position = CGPoint.zero
+        anchorPoint = CGPoint.zero
+    }
+
+    override init(layer: Any) {
+        super.init(layer: layer)
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(in ctx: CGContext) {
+        ctx.setFillColor(UIColor.red.cgColor)
+        let bezierPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        ctx.addPath(bezierPath.cgPath)
+        ctx.fillPath()
+    }
+
 }
 
-fileprivate extension SCDeviceType {
+
+class RoundedRectangleMaskLayer: CAShapeLayer {
     
-    var image: UIImage? {
-        switch self {
-        case .mynt:
-            return UIImage(named: "list_mynt")
-        case .myntGPS:
-            return UIImage(named: "list_gps")
-        case .myntES:
-            return UIImage(named: "list_es")
-        case .none:
-            return nil
+    override var cornerRadius: CGFloat {
+        didSet {
+            setNeedsDisplay()
+            layoutIfNeeded()
         }
     }
+
+    override var bounds: CGRect {
+        didSet {
+            setNeedsDisplay()
+            layoutIfNeeded()
+        }
+    }
+
+    override init() {
+        super.init()
+        contentsScale = UIScreen.main.scale
+        position = CGPoint.zero
+        anchorPoint = CGPoint.zero
+    }
+
+    override init(layer: Any) {
+        super.init(layer: layer)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(in ctx: CGContext) {
+        ctx.setFillColor(UIColor.red.cgColor)
+        let bezierPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        ctx.addPath(bezierPath.cgPath)
+        ctx.fillPath()
+    }
 }
+
+
