31d282780a414db37eaaf698310d60dafe39e469
JimmyLee05 <NanJun05@163.com>
2018-01-19 17:54:31 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,15 +1,13 @@
-// 版本信息
-versionLabel                = UILabel(frame: CGRect(x: 0, y: -80, width: winSize.width, height: 20))
-versionLabel.text           = "\(UIApplication.appName) \(UIApplication.appVersion) + (v\(UIApplication.appBuildVersion))"
-versionLabel.textAlignment  = .center
-versionLabel.textColor      = UIColor.black
-versionLabel.font           = UIFont.boldSystemFont(ofSize: 12)
-tableView.addSubview(versionLabel)
+if let vc = storyboard?.instantiateViewController(withIdentifier: "navi") as? NaviVC {
+                
+                present(vc, animated: true, completion: {
+                    self.stopParse()
+                })
+            }
 
-// 版本信息
-versionLabel                = UILabel(frame: CGRect(x: 0, y: -80, width: winSize.width, height: 20))
-versionLabel.text           = "\(UIApplication.appName) \(UIApplication.appVersion) + (v\(UIApplication.appBuildVersion))"
-versionLabel.textAlignment  = .center
-versionLabel.textColor      = UIColor.black
-versionLabel.font           = UIFont.boldSystemFont(ofSize: 12)
-tableView.addSubview(versionLabel)
+if let vc = storyboard?.instantiateViewController(withIdentifier: "navi") as? NaviVC {
+                
+                present(vc, animated: true, completion: {
+                    self.stopParse()
+                })
+            }
