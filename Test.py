f0463a337eea4779ea13406b7d8143d9c410ff53
JimmyLee05 <NanJun05@163.com>
2018-01-20 09:53:11 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,13 +1,13 @@
-if let vc = storyboard?.instantiateViewController(withIdentifier: "navi") as? NaviVC {
-                
-                present(vc, animated: true, completion: {
-                    self.stopParse()
-                })
-            }
+enum ParseState: String {
+        case none = "抓取微博视频"
+        case parsing = "查看已抓取视频"
+    }
+    
+    fileprivate var parseState: ParseState = .none
 
-if let vc = storyboard?.instantiateViewController(withIdentifier: "navi") as? NaviVC {
-                
-                present(vc, animated: true, completion: {
-                    self.stopParse()
-                })
-            }
+enum ParseState: String {
+        case none       = "抓去微博视频"
+        case parsing    = "查看已抓去视频"
+    }
+
+    fileprivate var parseState: ParseState = .none
