f1884bd32a57ab2a569b482e6025144a317d202a
JimmyLee05 <NanJun05@163.com>
2018-01-20 10:34:49 +0800

    add files

--- Test.py
+++ Test.py
@@ -1,13 +1,36 @@
-enum ParseState: String {
-        case none = "抓取微博视频"
-        case parsing = "查看已抓取视频"
+@objc fileprivate func weiboLinkDetected() {
+        guard let url = UIPasteboard.general.string?.weiboUrl(),
+            url.absoluteString != ignoredLink else {
+            return
+        }
+        let alert = WBDAlertController(title: "检测到剪贴板中有微博链接",
+                                       message: url.absoluteString)
+        alert.addAction(UIAlertAction(title: "抓取视频", style: .default, handler: { _ in
+            self.tf.text = url.absoluteString
+            self.parseState = .none
+            self.parse()
+        }))
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: { _ in
+            ignoredLink = url.absoluteString
+        }))
+        present(alert, animated: true, completion: nil)
     }
-    
-    fileprivate var parseState: ParseState = .none
 
-enum ParseState: String {
-        case none       = "抓去微博视频"
-        case parsing    = "查看已抓去视频"
-    }
-
-    fileprivate var parseState: ParseState = .none
+@objc fileprivate func weiboLinkDetected() {
+        
+        guard let url = UIPasteboard.general.string?.weiboUrl(),
+            url.absoluteString != ignoredLink else {
+            return
+        }
+        let alert = WBDAlertController(title: "检测到剪切板中有微博链接",
+                                       message: url.absoluteString)
+        alert.addAction(UIAlertAction(title: "抓取视频", style: .default, handler: { _ in
+            self.tf.text = url.absoluteString
+            self.parseState = .none
+            self.parse()
+        }))
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: { _ in
+            ignoredLink = url.absoluteString
+        }))
+        present(alert, animated: true, completion: nil)
+}
