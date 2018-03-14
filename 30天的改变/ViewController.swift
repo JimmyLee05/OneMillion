a87c8ae4c7e653c78d71f35725d3a906cde5e5d4
JimmyLee05 <NanJun05@163.com>
2018-03-14 10:49:36 +0800

    test git hook

--- 30天的改变/ViewController.swift
+++ 30天的改变/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarningsdajfhilkahkfhdajkfhjdksahfjkdhajkfdhajkfhdjsafhjdkdsaDdkjsfkjlDdajklfjkD jdkalfjkDjkdsa() {
+    override func didReceiveMemoryWarningsdajfhilkahkfhdajkfhjdksahfjkdhajkfdhajkfhdjsafhjdkdsaDdkjsfkjlDdajklfjkDjdkalfjkDjkdsa() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
